ee8da011fadd1995ebabafc4c8ac8b10789c3362
andresNeveu <vordones64@gmail.com>
2024-07-02 11:52:14 -0500

    Feat: inter font

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:local_notifier/local_notifier.dart';
 import 'package:pomo_app/pages/home.dart';
 import 'package:pomo_app/platform/database_provider.dart';
@@ -8,11 +10,16 @@ import 'package:window_manager/window_manager.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // Must add this line.
+
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+
   await windowManager.ensureInitialized();
 
   await localNotifier.setup(
-    appName: 'PomoApp',
+    appName: 'Pomo App',
     shortcutPolicy: ShortcutPolicy.requireCreate,
   );
 
--- lib/utils/theme_app.dart
+++ lib/utils/theme_app.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ThemeApp {
   static final theme = ThemeData(
+    textTheme: GoogleFonts.interTextTheme(),
     colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
     useMaterial3: true,
   );
